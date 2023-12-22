7a9c5095e189fc3c94d9748d0d991a227a10e9c7
Pavlo <winterspray@gmail.com>
2023-12-21 19:38:19 -0500

    dd

--- src/pages/home/Contact/Contact.js
+++ src/pages/home/Contact/Contact.js
@@ -9,7 +9,7 @@ const Contact = () => {
 					<div>
 						<iframe
 							src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3021.2099067969857!2d-73.9848203240614!3d40.779399233487005!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c2588a0502876d%3A0xc846668cdc7ff044!2s2091%20Broadway%2C%20New%20York%2C%20NY%2010023!5e0!3m2!1sru!2sus!4v1700182592093!5m2!1sru!2sus'
-							width={`${window.innerWidth - 300}px`}
+							width='100%'
 							height='300'
 							style={{ border: 0 }}
 							allowfullscreen=''
